b86fb0948bba43fef4c064b2d0ddc956828d88db
PabloBonet <pablo94_88@hotmail.com>
2024-06-15 13:21:33 -0300

    formularios_simples

--- src/Controller/FormulariosController.php
+++ src/Controller/FormulariosController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class FormulariosController extends AbstractController
 {
@@ -14,5 +18,33 @@ class FormulariosController extends AbstractController
         return $this->render('formularios/index.html.twig', [
             'controller_name' => 'FormulariosController',
         ]);
+    }
+
+    #[Route('/formularios/simple', name: 'formulario_simple')]
+    public function simple(Request $request): Response
+    {
+        $form = $this->createFormBuilder(null)
+        ->add('nombre', TextType::class, ['label'=>'Nombre'])
+        ->add('correo', TextType::class, ['label'=>'E-Mail'])
+        ->add('telefono', TextType::class, ['label'=>'Telefono'])
+        ->add('save', SubmitType::class)
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted())
+        {   //Si viene la peticion POST del formulario
+
+            $campos = $form->getData();
+            //print_r($campos);
+
+            echo "Nombre:".$campos['nombre']." | E-Mail:".$campos['correo']." | Telefono:".$campos['telefono'];
+            die();
+
+        }else
+        {
+
+        }
+        return $this->render('formularios/simple.html.twig', compact('form'));
     }
 }
